7834fce29486e64500a29456173bc1864df25232
Aguibou <barrybagata96@gmail.com>
2022-03-04 22:47:08 +0100

    notification customized

---
+++ Notifications/Notification.cs
@@ -0,0 +1,9 @@
+namespace GraphQl_app.Notifications
+{
+    public class Notification
+    {
+        public string TypeNotif { get; set; }
+       public int AbonnementId { get; set; }
+       public int ClientId { get; set; }
+    }
+}
